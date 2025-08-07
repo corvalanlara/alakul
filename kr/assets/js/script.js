f8e8d2c63c901e304f8204d506432b070c2997f4
Daniel Corvalán <daniel@corvalan.dev>
2025-08-07 02:41:49 -0400

    first deployment

---
+++ kr/assets/js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    const $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
+
+if (document.URL.includes("#sent")) {
+	document.querySelector('.notification').classList.remove('is-hidden')
+}
